c4b6a4ed062229b22cb13a5252354ec9d0058a48
senadakadric <skadric@fordham.edu>
2020-09-13 22:06:36 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,5 @@
 
 const superbowlWin = (arrayOfObjects) => {
   let winningObject = arrayOfObjects.find((obj) => obj.result === "W");
-  console.log(winningObject);
   return winningObject ? winningObject.year : undefined;
 };
