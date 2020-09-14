e7d2ed4df4eb6d39823093aa311870f175448534
senadakadric <skadric@fordham.edu>
2020-09-13 22:06:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// const testVar = {}
-
-// function testFunc() {
-//   return "hi"
-// }
-
 const superbowlWin = (arrayOfObjects) => {
   let winningObject = arrayOfObjects.find((obj) => obj.result === "W");
   return winningObject ? winningObject.year : undefined;
